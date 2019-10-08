95538cfa9ccfb01c83ae8241d112d3d1edb5ab08
Woqurefan <1074321997@qq.com>
2019-10-08 17:58:52 +0800

    Add files via upload

--- wqrfnium/test.py
+++ wqrfnium/test.py
@@ -1,17 +1,13 @@
-# -*- coding:utf-8 -*-
 import sys,requests,json
-reload(sys) # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+reload(sys)
 sys.setdefaultencoding('utf-8')
 import re,time
 from selenium import webdriver
-from wqrf_selenium import *
-#------------------------公共工具函数
+from wqrfnium import *
 
 driver = webdriver.Chrome()
 driver.get("http://wwww.baidu.com/")
 time.sleep(2)
-
-
 getelement(driver,"kw").send_keys('xiaozhu')
 
 
