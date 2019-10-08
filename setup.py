0992e1ca13c8779f94b5b4f3e136d0fa4bbfa49b
Woqurefan <1074321997@qq.com>
2019-10-08 15:01:50 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: wqrf
+# Mail: 1074321997@qq.com
+# Created Time:  2019-10-08 14:39:00
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "wqrf-selenium",
+    version = "0.1.0",
+    keywords = ("pip", "pathtool","timetool", "magetool", "mage"),
+    description = "to fix find element",
+    long_description = "if you can't find element,please use this",
+    license = "MIT Licence",
+
+    url = "https://github.com/Woqurefan/wqrf-selenium",
+    author = "wqrf",
+    author_email = "1074321997@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['selenium','Levenshtein','xlrd','xlutils']
+)
